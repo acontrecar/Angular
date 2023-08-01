4b25cf708f8c7209fb401deed7b37dfcb4b8cb82
AntonioContrerasC <acontrecar@gmail.com>
2023-08-02 00:23:10 +0200

    Update

--- 02-bases/src/app/heroes/list/list.component.ts
+++ 02-bases/src/app/heroes/list/list.component.ts
@@ -5,4 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  public heroeNames: string[] = [
+    'ironman',
+    'spiderman',
+    'hulk',
+    'thor',
+    'capitan america',
+  ];
+
+  public deletedHeroe?: string = '';
+
+  removeLastHeroe(): void {
+    this.deletedHeroe = this.heroeNames.pop();
+  }
+}
